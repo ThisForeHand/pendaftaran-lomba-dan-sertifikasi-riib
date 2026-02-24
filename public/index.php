61a43d606bd92e258ef70f1076dad20cebec0543
ThisForeHand <agil.farhandani9449@gmail.com>
2026-02-24 16:45:18 +0700

    Fix Laravel entrypoint paths in public index

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../../laravel_apps/pendaftaran/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/../../laravel_apps/pendaftaran/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../../laravel_apps/pendaftaran/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
